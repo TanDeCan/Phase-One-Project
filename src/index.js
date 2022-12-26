0c1a946cb016f73d300dca13611e4a70961c709c
VictoriaBladow <vbladow9999@yahoo.com>
2022-12-25 19:46:35 -0600

    got a list but no info added to the list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import weather from './db.json' assert { type: 'json' };
+document.addEventListener('DOMContentLoaded', () => {
+function renderWeather(date){
 
-console.log(weather);
+    const li = document.createElement('li');
+    li.classList.add('list-li');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = weather.DayOfWeek;
+
+    const pCon = document.createElement('p');
+    pCon.textContent = weather.Condition;
+
+    const pHiTemp = document.createElement('p');
+    pHiTemp.textContent = weather.HighTemp;
+
+    const pLowTemp = document.createElement('p');
+    pLowTemp.textContent = weather.LowTemp;
+
+    li.append(h3, pCon, pHiTemp, pLowTemp)
+    document.querySelector('#daily-weather').append(li);
+}
+
+weather.forEach(date => {
+    renderWeather(date);});
+
+})
+
+
+
+
+
+
